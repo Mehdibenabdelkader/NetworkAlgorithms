9e1a907152696f3f22b20213923cd05184fd6d90
Fluka <m.benabdelkader02@gmail.com>
2024-07-08 09:53:06 +0100

    initial commit

---
+++ shortestPath/djikstra.py
@@ -0,0 +1,26 @@
+import heapq
+
+def dijkstra(graph, start):
+    priority_queue = []
+    heapq.heappush(priority_queue, (0, start))  # (cost, node)
+
+    distances = {node: float('infinity') for node in graph}
+    distances[start] = 0
+
+    previous_nodes = {node: None for node in graph}
+
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous_nodes[neighbor] = current_node
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    return distances, previous_nodes
